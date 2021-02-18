59b171838f5f74d1e72abac13e72c87f50f52e78
magmagcup <sirawich.d@ku.th>
2021-02-18 09:34:24 +0700

    Initial push-- Config security

---
+++ src/main/java/ku/message/respository/MessageRepository.java
@@ -0,0 +1,9 @@
+package ku.message.respository;
+
+import ku.message.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
