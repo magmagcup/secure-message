6891153655f9d8ca3f49dbe15e3bc69b1ab7a476
magmagcup <sirawich.d@ku.th>
2021-02-18 11:58:33 +0700

    Finish OAuth(GitHub)

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package ku.message.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -18,26 +20,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/home", "/css/**", "/js/**").permitAll()
-                .anyRequest().authenticated();
-        http.formLogin()
-                .defaultSuccessUrl("/message", true)
-                .and().logout();
-    }
+                .anyRequest().authenticated()
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("usa")
-                .password(encoder().encode("usa"))
-                .roles("USER");
-    }
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+                .and()
+                .oauth2Login()
+                .defaultSuccessUrl("/home").permitAll()
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/home").permitAll();
     }
 
 }
+
 
 
 
